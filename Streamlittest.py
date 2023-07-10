f3ce23fce9a0e048ea29f68da5856d43346e7f8a
vijayan2004 <106367858+vijayan2004@users.noreply.github.com>
2023-07-10 16:23:31 +0530

    Final Commit

--- Streamlittest.py
+++ Streamlittest.py
@@ -1,29 +1,39 @@
 import streamlit as st
+import numpy as np 
+from PIL import Image 
+import cv2 
 
-import cv2
-import matplotlib.pyplot as plt
-plt.style.use('seaborn')
+def dodgeV2(x, y):
+    return cv2.divide(x, 255 - y, scale=256)
 
-img = st.file_uploader("Select a Image",type=['jpg','png','webp'])
-
-if st.button('Submit'):
-  img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  img_invert = cv2.bitwise_not(img_gray)
-  img_smoothing = cv2.GaussianBlur(img_invert, (21, 21),sigmaX=0, sigmaY=0)
-  final = cv2.divide(img_gray, 255 - img_smoothing, scale=255)
-  plt.figure(figsize=(8,8))
-  plt.imshow(final,cmap="gray")
-  plt.axis("off")
-  plt.title("Final Sketch Image")
-  st.pyplot(plt.show())
+def pencilsketch(inp_img):
+    img_gray = cv2.cvtColor(inp_img, cv2.COLOR_BGR2GRAY)
+    img_invert = cv2.bitwise_not(img_gray)
+    img_smoothing = cv2.GaussianBlur(img_invert, (21, 21),sigmaX=0, sigmaY=0)
+    final_img = dodgeV2(img_gray, img_smoothing)
+    return(final_img)
 
 
-#img = cv2.imread("C:\\Users\\GF LAB\\Pictures\\1025273-capts.webp")
+st.title("PencilSketch App")
+st.write("This Web App is to help convert your photos to realistic Pencil Sketches") 
 
-#img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-#plt.figure(figsize=(8,8))
-#plt.imshow(img)
-#plt.axis("off")
-#plt.title("Original Image")
-#plt.show()
+file_image = st.sidebar.file_uploader(label = "Take a pic of you to be sketched out")
 
+if file_image:
+    input_img = Image.open(file_image)
+    final_sketch = pencilsketch(np.array(input_img))
+    one, two = st.columns(2)
+    with one:
+        st.write("**Input Photo**")
+        st.image(input_img, use_column_width=True)
+    with two:
+        st.write("**Output Pencil Sketch**")
+        st.image(final_sketch, use_column_width=True)
+    if st.button("Download Sketch Images"):
+        im_pil = Image.fromarray(final_sketch)
+        im_pil.save('final_image.jpeg')
+        st.write('Download completed')
+   
+
+else:
+     st.write("You haven't uploaded any image file")
